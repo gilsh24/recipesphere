1ba2b9390c1ac72fd1e283b6199aad64e3a70c31
amami13 <avisar.amami@gmail.com>
2025-03-16 21:06:41 +0200

    Added first settings for firebase store and added registration page

--- app/src/main/java/com/example/recipesphere/ui/register/RegistrationActivity.kt
+++ app/src/main/java/com/example/recipesphere/ui/register/RegistrationActivity.kt
@@ -1,16 +1,18 @@
 package com.example.recipesphere.ui.register
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recipesphere.databinding.ActivityRegistrationBinding
 import com.example.recipesphere.ui.register.RegistrationViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.recipesphere.MainActivity
 
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistrationBinding
-    private lateinit var viewModel: RegistrationViewModel // Declare as a class property
+    private lateinit var viewModel: RegistrationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +39,11 @@ class RegistrationActivity : AppCompatActivity() {
         viewModel.registrationResult.observe(this) { result ->
             if (result.isSuccess) {
                 Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
-                finish()
+
+                // change to another page - like browse recipe
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
             } else {
                 Toast.makeText(this, "Registration failed: ${result.exceptionOrNull()?.message}", Toast.LENGTH_SHORT).show()
             }
